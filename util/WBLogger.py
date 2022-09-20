7160c4769fb6bd6c8544fde31c0c4c91cc4971c8
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-09-20 11:23:33 +0200

    Update WBLogger.py

--- util/WBLogger.py
+++ util/WBLogger.py
@@ -1,4 +1,5 @@
 import wandb
+import logging
 
 class LogerWB:
     def __init__(self, name, print_messages=False):
@@ -11,13 +12,13 @@ class LogerWB:
         wandb.log({"epoch_train_loss_adversarial": loss})
 
         if(self.print_messages):
-            print('Epoch[' + str(epoch) + '] loss_adversarial:' + str(loss))
+            logging.info('Epoch[' + str(epoch) + '] loss_adversarial:' + str(loss))
 
     def log_loss_batch_train_adversarial(self, length, epoch, batch_id, loss):
         wandb.log({"batch_train_loss_adversarial": loss})
 
         if(self.print_messages):
-            print('Epoch[' + str(epoch) + '] ' + 'Finished[' + str(batch_id / length * 100)[:5] + '%]' + 'Batch[' + str(batch_id) + ']' + 'loss_adversarial:' + str(loss))
+            logging.debug('Epoch[' + str(epoch) + '] ' + 'Finished[' + str(batch_id / length * 100)[:5] + '%]' + 'Batch[' + str(batch_id) + ']' + 'loss_adversarial:' + str(loss))
 
     #epoch
     def log_epoch(self, epoch):
@@ -32,66 +33,66 @@ class LogerWB:
             wandb.log({"epoch_train_acc_adversarial": loss})
 
             if(self.print_messages):
-                print('Epoch[' + str(epoch) + '] acc_adversarial:' + str(loss))
+                logging.info('Epoch[' + str(epoch) + '] acc_adversarial:' + str(loss))
 
     def log_acc_batch_train_adversarial(self, length, epoch, batch_id, loss):
         wandb.log({"batch_train_acc_adversarial": loss})
 
         if(self.print_messages):
-            print('Epoch[' + str(epoch) + '] ' + 'Finished[' + str(batch_id / length * 100)[:5] + '%]' + 'Batch[' + str(batch_id) + ']' + 'acc_adversarial:' + str(loss))
+            logging.debug('Epoch[' + str(epoch) + '] ' + 'Finished[' + str(batch_id / length * 100)[:5] + '%]' + 'Batch[' + str(batch_id) + ']' + 'acc_adversarial:' + str(loss))
 
     #Train iou adversarial
     def log_iou_epoch_train_adversarial(self, epoch, loss):
         wandb.log({"epoch_train_iou_adversarial": loss})
 
         if(self.print_messages):
-            print('Epoch[' + str(epoch) + '] iou_adversarial:' + str(loss))
+            logging.info('Epoch[' + str(epoch) + '] iou_adversarial:' + str(loss))
 
     def log_iou_batch_train_adversarial(self, length, epoch, batch_id, loss):
         wandb.log({"batch_train_iou_adversarial": loss})
 
         if(self.print_messages):
-            print('Epoch[' + str(epoch) + '] ' + 'Finished[' + str(batch_id / length * 100)[:5] + '%]' + 'Batch[' + str(batch_id) + ']' + 'iou_adversarial:' + str(loss))
+            logging.debug('Epoch[' + str(epoch) + '] ' + 'Finished[' + str(batch_id / length * 100)[:5] + '%]' + 'Batch[' + str(batch_id) + ']' + 'iou_adversarial:' + str(loss))
 
     #Vall loss
     def log_loss_epoch_val(self, epoch, loss):
         wandb.log({"epoch_val_loss": loss})
 
         if(self.print_messages):
-            print('Val[' + str(epoch) + '] loss:' + str(loss))
+            logging.info('Val[' + str(epoch) + '] loss:' + str(loss))
 
     #Vall iou
     def log_iou_epoch_val(self, epoch, loss):
         wandb.log({"epoch_val_iou": loss})
 
         if(self.print_messages):
-            print('Val_iou[' + str(epoch) + ' iou:' + str(loss))
+            logging.info('Val_iou[' + str(epoch) + ' iou:' + str(loss))
 
     #Vall acc
     def log_acc_epoch_val(self, epoch, loss):
         wandb.log({"epoch_val_acc": loss})
 
         if(self.print_messages):
-            print('Val_iou[' + str(epoch) + ' acc:' + str(loss))
+            logging.info('Val_iou[' + str(epoch) + ' acc:' + str(loss))
 
     #Vall loss_adversarial
     def log_loss_epoch_val_adversarial(self, epoch, loss):
         wandb.log({"epoch_val_loss_adversarial": loss})
 
         if(self.print_messages):
-            print('Val[' + str(epoch) + '] loss_adversarial:' + str(loss))
+            logging.info('Val[' + str(epoch) + '] loss_adversarial:' + str(loss))
 
     #Vall iou_adversarial
     def log_iou_epoch_val_adversarial(self, epoch, loss):
         wandb.log({"epoch_val_iou_adversarial": loss})
 
         if(self.print_messages):
-            print('Val_iou[' + str(epoch) + ' iou_adversarial:' + str(loss))
+            logging.info('Val_iou[' + str(epoch) + ' iou_adversarial:' + str(loss))
 
     #Vall acc_adversarial
     def log_acc_epoch_val_adversarial(self, epoch, loss):
         wandb.log({"epoch_val_acc_adversarial": loss})
 
         if(self.print_messages):
-            print('Val_iou[' + str(epoch) + ' acc_adversarial:' + str(loss))
+            logging.info('Val_iou[' + str(epoch) + ' acc_adversarial:' + str(loss))
 
